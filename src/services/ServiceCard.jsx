42a5b9361c44b964a5e0dca2d6d66988054932da
Mubasshir Ahmed <mubasshir3712@gmail.com>
2025-03-20 23:48:55 +0530

    change home page

--- src/services/ServiceCard.jsx
+++ src/services/ServiceCard.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import './service-card.css'
+import React from 'react';
+import { FaMotorcycle, FaPaintRoller, FaTools, FaMoneyBillWave } from 'react-icons/fa';
+import './service-card.css';
+
+const icons = {
+   'Wide Range of Bikes': <FaMotorcycle size={40} />,
+   'Bike Customization': <FaPaintRoller size={40} />,
+   'Servicing & Maintenance': <FaTools size={40} />,
+   'Easy Financing': <FaMoneyBillWave size={40} />,
+};
 
 const ServiceCard = ({ item }) => {
-   const { icons, title, desc } = item
+   const { title, desc } = item;
 
    return (
       <div className='service__item'>
-         <div className="service__img">
-            <img src={icons} alt="" />
+         <div className="service__icon">
+            {icons[title] || <FaMotorcycle size={40} />}  
          </div>
          <h6>{title}</h6>
          <p>{desc}</p>
       </div>
-   )
-}
+   );
+};
 
-export default ServiceCard
+export default ServiceCard;

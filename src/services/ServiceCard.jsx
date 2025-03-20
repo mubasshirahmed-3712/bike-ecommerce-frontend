9f8829abe1052723e5a3c90614486573bfd5a5f2
Mubasshir Ahmed <mubasshir3712@gmail.com>
2025-03-20 23:43:02 +0530

    change home page

--- src/services/ServiceCard.jsx
+++ src/services/ServiceCard.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import './service-card.css'
 
 const ServiceCard = ({ item }) => {
-   const { imgUrl, title, desc } = item
+   const { icons, title, desc } = item
 
    return (
       <div className='service__item'>
          <div className="service__img">
-            <img src={imgUrl} alt="" />
+            <img src={icons} alt="" />
          </div>
          <h6>{title}</h6>
          <p>{desc}</p>

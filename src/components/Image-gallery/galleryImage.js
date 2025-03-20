63b81f34fc3da2836743d0f2ea1db7976356b12a
Mubasshir Ahmed <mubasshir3712@gmail.com>
2025-03-21 01:20:18 +0530

    changes in new homepage

--- src/components/Image-gallery/galleryImage.js
+++ src/components/Image-gallery/galleryImage.js
@@ -1,12 +1,18 @@
 import galleryImg01 from '../../assets/images2/bike2.jpg'
 import galleryImg02 from '../../assets/images2/bike3.jpeg'
 import galleryImg03 from '../../assets/images2/bike8.jpg'
-import galleryImg04 from '../../assets/images2/bike12.jpg'
+import galleryImg04 from '../../assets/images2/bike9.jpg'
 import galleryImg05 from '../../assets/images2/bike11.jpg'
 import galleryImg06 from '../../assets/images2/bike14.jpg'
 import galleryImg07 from '../../assets/images2/bike15.jpeg'
 import galleryImg08 from '../../assets/images2/bike5.jpg'
-import galleryImg09 from '../../assets/images2/bike9.jpg'
+import galleryImg09 from '../../assets/images2/bike13.jpg'
+import galleryImg10 from '../../assets/images2/bike1.jpg'
+import galleryImg11 from '../../assets/images2/bike4.jpg'
+import galleryImg12 from '../../assets/images2/bike5.jpg'
+import galleryImg13 from '../../assets/images2/bike6.jpg'
+import galleryImg14 from '../../assets/images2/bike7.jpg'
+import galleryImg15 from '../../assets/images2/bike10.jpg'
 
 
 
@@ -15,12 +21,17 @@ const galleryImages = [
    galleryImg02,
    galleryImg03,
    galleryImg04,
-   galleryImg09,
-
    galleryImg05,
    galleryImg06,
    galleryImg07,
-   galleryImg08
+   galleryImg08,
+   galleryImg09,
+   galleryImg10,
+   galleryImg11,
+   galleryImg12,
+   galleryImg13,
+   galleryImg14,
+   galleryImg15,
 ]
 
 export default galleryImages

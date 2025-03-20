b85776016cccae3b894ecad48e715ab5fd8beb4e
Mubasshir Ahmed <mubasshir3712@gmail.com>
2025-03-21 00:27:05 +0530

    change home page

--- src/components/Image-gallery/galleryImage.js
+++ src/components/Image-gallery/galleryImage.js
@@ -1,12 +1,12 @@
 import galleryImg01 from '../../assets/images2/bike2.jpg'
 import galleryImg02 from '../../assets/images2/bike3.jpeg'
 import galleryImg03 from '../../assets/images2/bike8.jpg'
-import galleryImg04 from '../../assets/images2/bike9.jpg'
+import galleryImg04 from '../../assets/images2/bike12.jpg'
 import galleryImg05 from '../../assets/images2/bike11.jpg'
 import galleryImg06 from '../../assets/images2/bike14.jpg'
 import galleryImg07 from '../../assets/images2/bike15.jpeg'
 import galleryImg08 from '../../assets/images2/bike5.jpg'
-import galleryImg09 from '../../assets/images2/bike12.jpg'
+import galleryImg09 from '../../assets/images2/bike9.jpg'
 
 
 

49213d9a00bd213d9c0443ac54d420dc139cb1fe
Mubasshir Ahmed <mubasshir3712@gmail.com>
2025-03-20 22:02:13 +0530

    change in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/Home.css';
 import { Container, Row, Col } from 'reactstrap';
-import heroVideo1 from '../assets/videos/bike1.mp4';
-import heroVideo2 from '../assets/videos/bike2.mp4';
-import heroVideo3 from '../assets/videos/bike3.mp4';
+import heroVideo1 from '../assets/images2/bike-video2.mp4';
+import heroVideo2 from '../assets/images2/bike-video2.mp4';
+import heroVideo3 from '../assets/images2/bike-video2.mp4';
 
 const Home = () => {
    return (
